2c217179acaa22528f5f42beac487adfb9c2d98a
abdel-boudaoud <abdelkaderfarm@gmail.com>
2023-12-01 19:11:08 +0100

    day 1 ~ part 2 solution in JS

--- day-1/part-1.js
+++ day-1/part-1.js
@@ -1,40 +1,30 @@
 const fs = require("fs");
 
+let text = fs.readFile("day-1/input.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    getDigits(data);
+  }
+});
 
-let text = fs.readFile("day-1/input.txt", 'utf-8', (err, data)=>{
-    if(err){
-        console.log(err)
-    }else{
-        getDigits(data)
+let getDigits = (input) => {
+  let inputArr = input.split("\n");
+  let calibrations = [];
+
+  inputArr.forEach((ele) => {
+    let numbers = [];
+    for (let i = 0; i < ele.length; i++) {
+      if (Number(ele[i])) {
+        numbers.push(ele[i]);
+      }
     }
-})
+    calibrations.push(numbers);
+  });
 
-
-let getDigits = (input)=>{
-    let inputArr = input.split("\n")
-    let calibrations = []
-    
-    inputArr.forEach(ele=>{
-        let numbers = []
-        for(let i =0; i < ele.length;i++){
-            
-            if(Number(ele[i])){
-                numbers.push(ele[i])
-                
-            }
-            
-        }
-        calibrations.push(numbers)
+  let answer = calibrations
+    .map((x) => {
+      return parseInt(x[0] + x[x.length - 1], 10);
     })
-
-    let ff = calibrations.map(x=>{
-        return parseInt(x[0] + x[x.length-1], 10) 
-    })
-
-    console.log(ff.reduce((a, b)=> a+b  , 0)) //answer was 54968
-
-}
-
-
-
-
+    .reduce((a, b) => a + b, 0);
+};
